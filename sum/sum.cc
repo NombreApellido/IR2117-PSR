d9a3463fca5fe198d2fe28ee5432d2c8d9fdd929
NombreApellido <al416816@uji.es>
2023-01-26 12:52:46 +0100

    sum - Version 1

---
+++ sum/sum.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Enter a number: ";
+    int a;
+    int sum=0;
+    cin >> a;
+    for(int i = 1; i <= sum; i=i+1){
+        sum = sum + i;
+    }
+    cout<<"The sum from 1 to "<< a<<" is "<< sum << endl;
+
+    return 0;
+}
+
