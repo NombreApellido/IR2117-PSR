0dac9cdd51dea574bcb6a7218e225fbf47bfd1cd
NombreApellido <al416816@uji.es>
2023-01-26 12:56:09 +0100

    sum - Version 2

--- sum/sum.cc
+++ sum/sum.cc
@@ -8,11 +8,12 @@ int main()
     int a;
     int sum=0;
     cin >> a;
-    for(int i = 1; i <= sum; i=i+1){
+    for(int i = 1; i <= a; i=i+1){
         sum = sum + i;
     }
     cout<<"The sum from 1 to "<< a<<" is "<< sum << endl;
 
     return 0;
 }
+
 
