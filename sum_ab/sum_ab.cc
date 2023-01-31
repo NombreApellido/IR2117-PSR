55dc073af2eac00ed3efbd373a5e3d94b7a749e1
Marc <al416816@uji.es>
2023-01-31 11:22:01 +0100

    v2

--- sum_ab/sum_ab.cc
+++ sum_ab/sum_ab.cc
@@ -1,21 +1,28 @@
 #include <iostream>
 
-using namespace std;
+int Sumv3(int a, int b){
+    int sum = 0;
+    for(int i= a; i <= b; i++){
+        sum = sum+i;
+    }
+    return sum;
+}
 
 int main()
 {
-
-    int a, b;
-    int sum = 0;
-    cout << "Enter a: ";
-    cin >> a;
-    cout << "Enter b: ";
-    cin>> b;
-    for(int i = a; i<=b;i++)
-    {
-        sum += i;
-    
+    int a, b, sum= 0;
+    do{
+        std::cout << "Enter a number higher than 0 for a: ";
+        std::cin >> a;
     }
-    cout << "The sum is " << sum;
+    while(a < 1);
+    do{
+        std::cout << "Enter a number higher than 0 for b: ";
+        std::cin >> b;
+    }
+    while(b < 1);
+    sum = Sumv3(a, b);
+    std::cout<< "La suma entre " << a << " y " << b << " es :" << sum;
+    
     return 0;
 }
