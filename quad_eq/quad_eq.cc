3962444b7cd78ac10240451ccddac7b6a405f9a0
NombreApellido <al416816@uji.es>
2023-02-07 19:50:17 +0100

    v4 single solution

--- quad_eq/quad_eq.cc
+++ quad_eq/quad_eq.cc
@@ -14,11 +14,15 @@ int main()
     {
         cout << "Discriminant below 0, try again"   ; 
     }
+    if(b*b-4*a*c == 0)
+    {
+        cout << "x = 0" << endl;
+    }
     else
     {
         x1 = (-b+sqrt(b*b-4*a*c))/(2*a);
         x2 = (-b-sqrt(b*b-4*a*c))/(2*a);
-        cout << "x1 = " << x1 << endl;
+        
         cout << "x2 = " << x2 << endl;
   
     }
