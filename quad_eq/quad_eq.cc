dea9e27a0bf70cc26d7efc89b26d86c357dc2a5e
NombreApellido <al416816@uji.es>
2023-02-07 19:54:55 +0100

    v4 minor mistake solved

--- quad_eq/quad_eq.cc
+++ quad_eq/quad_eq.cc
@@ -22,7 +22,7 @@ int main()
     {
         x1 = (-b+sqrt(b*b-4*a*c))/(2*a);
         x2 = (-b-sqrt(b*b-4*a*c))/(2*a);
-        
+        cout << "x1 = " << x1 << endl;
         cout << "x2 = " << x2 << endl;
   
     }
