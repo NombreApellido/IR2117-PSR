0b97a3c4b8bf463c2520f30f7173c50088b175c1
NombreApellido <al416816@uji.es>
2023-02-07 12:50:27 +0100

    v1 quad_eq

---
+++ quad_eq/quad_eq.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a, b, c, x1, x2;
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+    cout << "Enter c: ";
+    cin >> c;
+    x1 = 0;
+    x2 = 0;
+    cout << "x1 = " << x1 << endl;
+    cout << "x2 = " << x2 << endl;
+
+    return 0;
+}
+
