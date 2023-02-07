139ff7886697de45edbaedf16696166f2f8978e4
NombreApellido <al416816@uji.es>
2023-02-07 20:23:49 +0100

    final version complex solutions

--- quad_eq/quad_eq.cc
+++ quad_eq/quad_eq.cc
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-    double a, b, c, x1, x2;
+    double a, b, c, x1, x2, dis1, dis2;
     cout << "Enter a: ";
     cin >> a;
     cout << "Enter b: ";
@@ -12,16 +12,30 @@ int main()
     cin >> c;
     if (b*b-4*a*c < 0)
     {
-        cout << "Discriminant below 0, try again"   ; 
+        x1 = -b/(2*a);
+        x2 = -b/(2*a);
+        dis1 = sqrt(-1*(b*b-4*a*c))/(2*a);
+        dis2 = sqrt(-1*(b*b-4*a*c))/(2*a);
+        if (dis1 > 0)
+        {
+        cout << "x2 = " << x2 << "-"<< dis2 << "i"<< endl;
+        cout << "x1 = " << x1 << "+"<< dis1 << "i" << endl;  
+        }
+        else
+        {
+        cout << "x1 = " << x1 << "-" << dis1 << "i"<< endl;
+        cout << "x2 = " << x2 << "+"<< dis2 << "i" << endl;  
+        }
     }
     if(b*b-4*a*c == 0)
     {
-        cout << "x = 0" << endl;
+        x1 = (-b+sqrt(b*b-4*a*c))/(2*a);
+        cout << "x = " << x1 << endl;
     }
-    else
+    if(b*b-4*a*c > 0)
     {
         x1 = (-b+sqrt(b*b-4*a*c))/(2*a);
-        x2 = (-b-sqrt(b*b-4*a*c))/(2*a);
+        x2= (-b-sqrt(b*b-4*a*c))/(2*a);
         cout << "x1 = " << x1 << endl;
         cout << "x2 = " << x2 << endl;
   
