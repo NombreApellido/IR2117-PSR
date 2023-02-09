9d726c69c609b85464bfbe8c3db608e1c2cae1c4
NombreApellido <al416816@uji.es>
2023-02-09 12:24:53 +0100

    v3 sum ros

--- ros2_ws/src/examples/sum/sum.cpp
+++ ros2_ws/src/examples/sum/sum.cpp
@@ -2,13 +2,18 @@
 #include "std_msgs/msg/int32.hpp"
 #include <iostream>
 
+int sum;
+
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {
+  sum += msg->data;
   std::cout << msg->data << std::endl;
+  
 }
 
 int main(int argc, char * argv[])
 {
+  sum = 0;
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("sum");
   auto subscription = node->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
