c47a323d40e434d5e1af8cde5ae8c9f410550988
NombreApellido <al416816@uji.es>
2023-02-14 19:22:59 +0100

    arith_mean v2

--- Statistics/arith_mean.cc
+++ Statistics/arith_mean.cc
@@ -4,8 +4,11 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    double m = 0;
-    
+    double m = 0, s=0;
+    int n=argc-1;
+    for (int i=1; i<=n; i++)
+    	s += argv[i];
+    m = s/n;
     cout << "Arithmetic mean: " << m << endl;
     
     return 0;
