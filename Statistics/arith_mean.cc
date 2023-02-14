311acf01e686ca7cb0e6407f3f7f650c8ce27fd5
NombreApellido <al416816@uji.es>
2023-02-14 19:40:17 +0100

    arith_mean v6

--- Statistics/arith_mean.cc
+++ Statistics/arith_mean.cc
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
     double m = 0, s=0;
     int n=0, element;
     cin >> element;
-    while (!cin.eof())
+    while (not cin.eof())
     {
     	n += 1;
     	s += element;
