960c8e4a6e36b58964128ed6a5b5b9cb1b90cb25
NombreApellido <al416816@uji.es>
2023-02-15 01:13:15 +0100

    arith_mean v7

--- Statistics/arith_mean.cc
+++ Statistics/arith_mean.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,13 +7,18 @@ int main(int argc, char** argv)
 {
     double m = 0, s=0;
     int n=0, element;
+    vector<int> elements;
     cin >> element;
     while (not cin.eof())
     {
+    	elements.push_back(element);
     	n += 1;
-    	s += element;
     	cin >> element;
     }	
+    for (int i=0; i<n; i++)
+    {
+    	s += elements[i];
+    }
     m = s/n;
     cout << "Arithmetic mean: " << m << endl;
     
