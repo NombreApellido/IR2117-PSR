37942a0c88a166bb932dae45fdbdde4f9c474d32
NombreApellido <al416816@uji.es>
2023-02-16 13:16:57 +0100

    square challenged finished

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -10,15 +10,36 @@ int main(int argc, char * argv[])
   auto node = rclcpp::Node::make_shared("publisher");
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
   geometry_msgs::msg::Twist message;
-  rclcpp::WallRate loop_rate(500ms);
-
-  while (rclcpp::ok()) {
-    message.linear.x = 1.0;
-    message.angular.z = 1.0;
+  rclcpp::WallRate loop_rate(10ms);
+  
+  for(int j=0; j<4; j++){
+    int i=0, n=1000;
+    while (rclcpp::ok() && (i<n)) {
+      i++;
+      // move forward
+      message.linear.x = 0.1;
+      publisher->publish(message);
+      rclcpp::spin_some(node);
+      loop_rate.sleep();
+    }
+    
+    message.linear.x = 0.0;
     publisher->publish(message);
-    rclcpp::spin_some(node);
-    loop_rate.sleep();
-  }
+    
+    i=0;
+    n=1571;
+    while (rclcpp::ok() && (i<n)) {
+      i++;
+      // trun
+      message.angular.z = 0.1;
+      publisher->publish(message);
+      rclcpp::spin_some(node);
+      loop_rate.sleep();
+    }
+    message.angular.z = 0;
+    publisher->publish(message);
+   }
+  // send zero velocity to topic
   rclcpp::shutdown();
   return 0;
 }
