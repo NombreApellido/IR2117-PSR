08002ec1c3b32788a71222a9de9dca326a44e5b6
NombreApellido <al416816@uji.es>
2023-02-21 21:06:49 +0100

    find maximum

--- Statistics/mode.cc
+++ Statistics/mode.cc
@@ -14,13 +14,27 @@ int main(int argc, char** argv) {
   }
   
   map<double,int> dicc;
-  int x=0;
-  for(int j=0; j<elements.size();j++){
-    if(elements[i]==elements[j]){
-      x++;
-    }
-    dicc[i]=x;
+  for(int i=0; i<elements.size();i++){
+    int x=0;
+      for(int j=0; j<elements.size();j++){
+        if(elements[i]==elements[j]){
+          x++;
+        }
+        dicc[i]=x;
+      }
   }
+  
+  int maxvalue=0;
+  double maxkey=0;
+
+  for(const auto&pair:dicc){
+    if(pair.second>maxvalue){
+      maxkey= pair.first;
+      maxvalue=pair.second;
+    }
+  }
+
+    m=maxkey;
   
   std:cout << "Mode: " << m << std:endl;
   return 0;
