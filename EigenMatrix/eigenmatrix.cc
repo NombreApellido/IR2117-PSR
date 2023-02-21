214216ffb106c4bce366a89c1778ff522b23897e
NombreApellido <al416816@uji.es>
2023-02-21 12:58:55 +0100

    eigenmatrix v1

---
+++ EigenMatrix/eigenmatrix.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+int main() {
+  
+  int filas, sumamax = 0;
+  std::cout << "Introduce el número de filas de la matriz cuadrada: " << std::endl;
+  cout << sumamax << std::endl;
+
+
+
+
+}
