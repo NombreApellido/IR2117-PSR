35ad6d460581b647d7f463952e6d9811181a5bc7
NombreApellido <al416816@uji.es>
2023-02-21 21:54:17 +0100

    case for odd size

--- Statistics/median.cc
+++ Statistics/median.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <bits/stdc++.h>
+
 int main(int argc, char** argv) {
   double m=0, s=0;
   int n=0, element;
@@ -11,7 +13,22 @@ int main(int argc, char** argv) {
     std:cin >> element;
     
   }
-  cout << "Number of elements: " << n << endl;
+  if(n%2!=0) {
+      m = elements[n/2];
+  }
+
+  cout<<"Number of elements: "<<n<<endl;
+  for(int i=0; i<sizeof(elements)-1; i++){
+    for (int j=0; j<n-i-1;j++){
+      if(elements[j]>elements[j+1]){
+        swap(elements[j], elements[j+1]);
+      }
+    }
+  }
+  cout<<"Sorted elements are: ";
+  for(int i=0; x<elements.size(); i++) {
+    cout << elements[i];
+  }
   std:cout << "Median: " << m << std:endl;
   return 0;
   
