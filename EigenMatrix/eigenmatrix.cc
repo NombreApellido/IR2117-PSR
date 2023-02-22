cd695bb493b59123a53ef2f45e08263d267f6eb6
NombreApellido <al416816@uji.es>
2023-02-22 18:19:04 +0100

    eigen matrix v1 (fixed)
    i think this is what the exercise asks for

--- EigenMatrix/eigenmatrix.cc
+++ EigenMatrix/eigenmatrix.cc
@@ -6,11 +6,20 @@ using Eigen::VectorXd;
 
 int main() {
   
-  int filas, sumamax = 0;
+  int nfilas, sumamax = 0;
   std::cout << "Introduce el número de filas de la matriz cuadrada: " << std::endl;
+  std::cin >> nfilas;
+  MatrixXd m(nfilas, nfilas); // Se crea la matriz deseada (?)
+  for(int f=0; f<=nfilas; f++) {
+    for(int c=0; c<=nfilas; c++) {
+      int dato;
+      std::cout << "Introduce el dato para la posición (" << f << ", " << c << ")" << std::endl;
+      std::cin >> dato;
+      m(f, c) = dato;
+    }
+  } 
   cout << sumamax << std::endl;
 
 
-
-
+  return 0;
 }
