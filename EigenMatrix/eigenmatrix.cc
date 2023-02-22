533073b5b560d7ec27b175c8b487c96116532968
NombreApellido <al416816@uji.es>
2023-02-22 18:47:45 +0100

    eigen v2

--- EigenMatrix/eigenmatrix.cc
+++ EigenMatrix/eigenmatrix.cc
@@ -5,19 +5,23 @@ using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
 int main() {
-  
-  int nfilas, sumamax = 0;
-  std::cout << "Introduce el número de filas de la matriz cuadrada: " << std::endl;
-  std::cin >> nfilas;
-  MatrixXd m(nfilas, nfilas); // Se crea la matriz deseada (?)
-  for(int f=0; f<=nfilas; f++) {
-    for(int c=0; c<=nfilas; c++) {
-      int dato;
-      std::cout << "Introduce el dato para la posición (" << f << ", " << c << ")" << std::endl;
-      std::cin >> dato;
-      m(f, c) = dato;
+  bool rep = true;
+  while (rep) {
+    int nfilas, sumamax = 0;
+    std::cout << "Introduce el número de filas de la matriz cuadrada: " << std::endl;
+    std::cin >> nfilas;
+    MatrixXd m(nfilas, nfilas); // Se crea la matriz deseada (?)
+    for(int f=0; f<=nfilas; f++) {
+      for(int c=0; c<=nfilas; c++) {
+        int dato;
+        std::cout << "Introduce el dato para la posición (" << f << ", " << c << ")" << std::endl;
+        std::cin >> dato;
+        m(f, c) = dato;
+      }
     }
-  } 
+    if(m.isSquare()) {
+      rep = false;
+  }
   cout << sumamax << std::endl;
 
 
