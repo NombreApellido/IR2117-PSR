b7d0704bdf1603d0ddba94dbffd4badeb5365ad8
NombreApellido <al416816@uji.es>
2023-02-24 17:45:50 +0100

    mode eigen v1 final

--- Statistics/mode.cc
+++ Statistics/mode.cc
@@ -2,43 +2,33 @@
 #include <iostream>
 #include <Eigen/Dense>
 
+
 int main(int argc, char** argv) {
-  double m=0, s=0;
+  
+  double m=0;
   int n=0, element;
-  std::vector<int> elements;
   std::cin >> element;
+  Eigen::VectorXd elements(100);
+  
   while (not std::cin.eof()) {
-    elements.push_back(element);
+    elements(n) = element;
     n += 1;
-    std:cin >> element;
-    
-  }
-  
-  map<double,int> dicc;
-  for(int i=0; i<elements.size();i++){
-    int x=0;
-      for(int j=0; j<elements.size();j++){
-        if(elements[i]==elements[j]){
-          x++;
-        }
-        dicc[i]=x;
-      }
-  }
-  
-  int maxvalue=0;
-  double maxkey=0;
-
-  for(const auto&pair:dicc){
-    if(pair.second>maxvalue){
-      maxkey= pair.first;
-      maxvalue=pair.second;
-    }
+    std::cin >> element;
   }
 
-    m=maxkey;
+  elements.conservativeResize(n);
+  Eigen::Map<Eigen::VectorXd> counts(elements.size());
   
-  std:cout << "Mode: " << m << std:endl;
+  for(int i=0; i < elements.size(); i++ ){
+    counts(i) = (elements.array() == elements(i)).count();
+  
+  }
+
+  int maxIndex;
+  double maxCount;
+  counts.maxCoeff(&maxIndex);
+  maxCount = counts(maxIndex);
+  m = elements(maxIndex);
+  std::cout << "Mode: " << m << std::endl;
   return 0;
-
-
 }
