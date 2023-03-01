d90e3bcb6f9a48af74b1e5fc15ae70b238a6aec3
NombreApellido <al416816@uji.es>
2023-03-01 23:36:05 +0100

    vector function with types changed

--- Statistics/median.cc
+++ Statistics/median.cc
@@ -2,6 +2,14 @@
 #include <Eigen/Dense>
 
 using Eigen::VectorXd;
+std::istream& operator>>(std::istream& in,
+			  VectorXd& v) {
+    double element;
+    in>>element;
+    while (not in.eof()){
+        v.push_back(element);
+        in >> element;
+    }			  
 
 int main() {
     VectorXd v;
--- Statistics/mode.cc
+++ Statistics/mode.cc
@@ -2,11 +2,17 @@
 #include <Eigen/Dense>
 
 using Eigen::VectorXd;
+std::istream& operator>>(std::istream& in,
+			  VectorXd& v) {
+    double element;
+    in>>element;
+    while (not in.eof()){
+        v.push_back(element);
+        in >> element;
+    }			  
 
 int main() {
     VectorXd v;
     std::cin >> v;
-
-
 
 }
