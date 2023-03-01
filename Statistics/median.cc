d778183027f2d0375d1a17627ab49567d0aa3009
NombreApellido <al416816@uji.es>
2023-03-01 23:16:06 +0100

    v0

--- Statistics/median.cc
+++ Statistics/median.cc
@@ -1,31 +1,11 @@
-#include "utils.cc"
 #include <iostream>
 #include <Eigen/Dense>
 
-int main(int argc, char** argv) {
-  double median=0, s=0;
-  int n=0, element;
-  Eigen::VectorXd elements(100);
-  std::cin >> element;
-  while (not std::cin.eof()) {
-    elements(n) = element;
-    n += 1;
-    std:cin >> element;
-    
-  }
-  elements.conservativeResize(n); // ajustamos el tamaño del vector
-  std::sort(elements.data(), elements.data()+n);
-  
-  if(n%2!=0) {
-      m = elements(n/2); 
-  }
-  else{
-    m=(elements(n/2)+elements(n/2+1))/2;
-  }
-  cout<<"Number of elements: " << n << endl;
-  cout<<"Sorted elements are: " << elements << std::endl;
-  std:cout << "Median: " << m << std:endl;
-  return 0;
-  
+using Eigen::VectorXd;
+
+int main() {
+
+
+
 
 }
--- Statistics/mode.cc
+++ Statistics/mode.cc
@@ -1,34 +1,11 @@
-#include "utils.cc"
 #include <iostream>
 #include <Eigen/Dense>
 
+using Eigen::VectorXd;
 
-int main(int argc, char** argv) {
-  
-  double m=0;
-  int n=0, element;
-  std::cin >> element;
-  Eigen::VectorXd elements(100);
-  
-  while (not std::cin.eof()) {
-    elements(n) = element;
-    n += 1;
-    std::cin >> element;
-  }
+int main() {
 
-  elements.conservativeResize(n);
-  Eigen::Map<Eigen::VectorXd> counts(elements.size());
-  
-  for(int i=0; i < elements.size(); i++ ){
-    counts(i) = (elements.array() == elements(i)).count();
-  
-  }
 
-  int maxIndex;
-  double maxCount;
-  counts.maxCoeff(&maxIndex);
-  maxCount = counts(maxIndex);
-  m = elements(maxIndex);
-  std::cout << "Mode: " << m << std::endl;
-  return 0;
+
+
 }
