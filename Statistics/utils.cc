c1da8ef1fe021c324a248704b384b5dd6327a67b
NombreApellido <al416816@uji.es>
2023-03-05 19:52:51 +0100

    compute median

--- Statistics/utils.cc
+++ Statistics/utils.cc
@@ -1,4 +1,5 @@
 #include "utils.hpp"
+#include <map>
 
 using Eigen::VectorXd;
 
@@ -9,4 +10,13 @@ int main() {
     std::sort(v.data(), v.data()+v.size());
     std::cout << "The vector is: " << std::endl;
     std::cout << v << std::endl;
+    int middle = v.size()/2;
+    double m;
+    if (v.size()%2==0){
+        m = (v[middle]+v[middle-1]/2;
+    }
+    else{
+        m = v[middle];
+    }
+    std::cout << "Median: " << m << std::endl;
 }
