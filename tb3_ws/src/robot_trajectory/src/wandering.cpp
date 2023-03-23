662746c4dbe2863eac796a8e0c41a8102616bac2
NombreApellido <al416816@uji.es>
2023-03-23 13:04:45 +0100

    wandering v2

--- tb3_ws/src/robot_trajectory/src/wandering.cpp
+++ tb3_ws/src/robot_trajectory/src/wandering.cpp
@@ -4,26 +4,31 @@
 #include "geometry_msgs/msg/twist.hpp"
 using namespace std::chrono_literals;
 
-void scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+void scan_callback(sensor_msgs::msg::LaserScan::SharedPtr msg)
 {
 
-
-
+    std::cout << msg->ranges[0] << std::endl;
+    std::cout << msg->ranges[90] << std::endl;
+    std::cout << msg->ranges[180] << std::endl;
+    std::cout << msg->ranges[270] << std::endl;
 
 }
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("wandering");
-  auto subscription = node->create_subscription<sensor_msgs::msg::LaserScan>("/scan", 10, scan_callback);
-  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 0);
+  auto node = rclcpp::Node::make_shared("publisher");
+  auto subscription = node->create_subscription<sensor_msgs::msg::LaserScan>("scan", 10, scan_callback);
+  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
 
   
   rclcpp::WallRate loop_rate(10ms);
-
+  geometry_msgs::msg::Twist message;
   while (rclcpp::ok()) {
-    
+      message.linear.x = 0.0;
+      publisher->publish(message);
+      rclcpp::spin_some(node);
+      loop_rate.sleep();
   }
   rclcpp::shutdown();
   return 0;
