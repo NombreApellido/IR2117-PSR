a4e6851bd2ca7e0e8a39e93812709638490809db
NombreApellido <al416816@uji.es>
2023-03-30 18:31:45 +0200

    subscribers added to avoidance v0

--- tb3_ws/src/obstacles/src/avoidance.cpp
+++ tb3_ws/src/obstacles/src/avoidance.cpp
@@ -4,11 +4,29 @@
 
 using namespace std::chrono_literals;
 
+void callback_front(const example_interfaces::msg::Bool::SharedPtr msg)
+{
+
+}
+
+void callback_left(const example_interfaces::msg::Bool::SharedPtr msg)
+{
+
+}
+
+void callback_right(const example_interfaces::msg::Bool::SharedPtr msg)
+{
+
+}
+
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("avoidance");
   auto publisher = node->create_publisher<geometry::msg::Twist>("cmd_vel", 10);
+  auto subscription = node->create_subscription<geometry::msg::Twist>("/front/obstacle", 10, callback_front);
+  auto subscription = node->create_subscription<geometry::msg::Twist>("/left/obstacle", 10, callback_left);
+  auto subscription = node->create_subscription<geometry::msg::Twist>("/right/obstacle", 10, callback_right);
   geometry_msgs::msg::Twist message;
   rclcpp::WallRate loop_rate(50ms);
   
