cd4280c57f9ffaca7fad1ac3ee341396e610e76b
NombreApellido <al416816@uji.es>
2023-04-20 13:09:00 +0200

    avoidance finished

--- tb3_ws/src/obstacles/src/avoidance.cpp
+++ tb3_ws/src/obstacles/src/avoidance.cpp
@@ -1,9 +1,11 @@
+#include <iostream>
+#include <string.h>
+#include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "example_interfaces/msg/bool.hpp"
-#include <chrono>
 
-enum State{
+enum State {
   S1,
   S2,
   S3
@@ -17,21 +19,21 @@ bool front_obstacle = false, left_obstacle = false, right_obstacle = false;
 
 void callback_front(const example_interfaces::msg::Bool::SharedPtr msg)
 {
-   front_obstacle = msg->data;
+	front_obstacle = msg->data; 
 }
-
 void callback_left(const example_interfaces::msg::Bool::SharedPtr msg)
 {
-   left_obstacle = msg->data;
+	left_obstacle = msg->data; 
 }
-
 void callback_right(const example_interfaces::msg::Bool::SharedPtr msg)
 {
-   right_obstacle = msg->data;
+	right_obstacle = msg->data; 
 }
 
-int main(int argc, char * argv[])
+   
+int main(int argc, char * argv[]) 	
 {
+
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("avoidance");
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
@@ -40,47 +42,47 @@ int main(int argc, char * argv[])
   auto subs_right = node->create_subscription<example_interfaces::msg::Bool>("/right/obstacle", 10, callback_right);
   geometry_msgs::msg::Twist message;
   rclcpp::WallRate loop_rate(50ms);
-  
+ 
+    
   while (rclcpp::ok()) {
-      
-      switch (state) {
-          case S1:
-              if (front_obstacle) {
-                  state = S2;
-              }
-              break;
-              
-          case S2:
-              if (left_obstacle and front_obstacle) {
-                  state = S3;
-              }else if (!front_obstacle){
-                  state = S1;
-              }
-              break;
-              
-          case S3:
-              if (!front_obstacle) {
-                  state = S1;
-              }
-              break;
-                  
-      }
-      if (state == S1) {
-          message.linear.x = 0.5;
-          message.linear.z = 0;
-      }else if (state == S2){
-          message.linear.x = 0;
-          message.linear.z = 0.5;
-      }else if (state == S3){
-          message.linear.x = 0;
-          message.linear.z = -0.5;
-      }
+	
+	switch (state) {
+    	case S1:
+      		if (front_obstacle) {
+        	state = S2;
+     		}
+      		break;
+    	case S2:
+      		if (left_obstacle & front_obstacle) {
+        		state = S3;
+      		} else if ( !front_obstacle ) {
+        		state = S1;
+      		}
+      		break;
+      	case S3:
+      		if (!front_obstacle) {
+        		state = S1;
+      		}
+      		break;
+  	}
+
+  	if (state == S1) {
+    	message.linear.x = 0.3;
+    	message.angular.z = 0;
+  	} else if (state == S2) {
+    	message.linear.x = 0;
+    	message.angular.z = 0.3;
+  	} else if (state == S3) {
+    	message.linear.x = 0;
+    	message.angular.z = -0.3;
+  	}
+
+  	publisher->publish(message);
+  	rclcpp::spin_some(node);
+  	loop_rate.sleep(); 
+	}
   
-      publisher->publish(message);
-      rclcpp::spin_some(node);
-      loop_rate.sleep();
-  }
+ 
   rclcpp::shutdown();
   return 0;
 }
-
